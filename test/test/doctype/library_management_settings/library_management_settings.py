32a9d490ed412f7da7d9c5598692d004f42c6309
murex9 <45913654+murex9@users.noreply.github.com>
2018-12-16 12:24:01 +0100

    Delete library_management_settings.py

--- test/test/doctype/library_management_settings/library_management_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2013, Frappe
-# For license information, please see license.txt
-
-from __future__ import unicode_literals
-import frappe
-from frappe.model.document import Document
-
-class LibraryManagementSettings(Document):
-	pass
